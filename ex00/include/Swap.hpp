b226677f7143c965354c83b41463d071c54d56e4
Saladin Afoh <safoh@student.codam.nl>
2023-07-19 15:48:38 +0200

    Completed ex00

---
+++ ex00/include/Swap.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    .--.  _                 */
+/*   Swap.hpp                                        |o_o || |                */
+/*                                                   |:_/ || |_ _   ___  __   */
+/*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
+/*                                                 (|     | )|_| |_| |>  <    */
+/*   Created: 2023/07/19 15:39:37 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
+/*   Updated: 2023/07/19 15:39:37 by safoh        \___)=(___/                 */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SWAP_HPP
+#define SWAP_HPP
+
+#include <Color.hpp>
+#include <Defines.hpp>
+
+template <typename T> void swap(T &a, T &b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+template <typename T> T min(T &a, T &b)
+{
+	return (a < b ? a : b);
+}
+
+template <typename T>
+
+T max(T &a, T &b)
+{
+	return (a > b ? a : b);
+}
+
+#endif
+
+/* ************************************************************************** */
---
+++ ex00/src/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                    .--.  _                 */
+/*   main.cpp                                        |o_o || |                */
+/*                                                   |:_/ || |_ _   ___  __   */
+/*   By: safoh <safoh@student.codam.nl>             //   \ \ __| | | \ \/ /   */
+/*                                                 (|     | )|_| |_| |>  <    */
+/*   Created: 2023/05/24 14:47:46 by safoh        /'\_   _/`\__|\__,_/_/\_\   */
+/*   Updated: 2023/05/24 14:47:46 by safoh        \___)=(___/                 */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <Swap.hpp>
+#include <cstdlib>
+#include <iostream>
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+	std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+	std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	return (EXIT_SUCCESS);
+}
+
+/* ************************************************************************** */
