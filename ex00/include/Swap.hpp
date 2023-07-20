4d1cc7a4b3b8df261d254c00761b9b08bde16faf
Saladin Afoh <safoh@student.codam.nl>
2023-07-20 14:35:06 +0200

    Formatting

--- ex00/include/Swap.hpp
+++ ex00/include/Swap.hpp
@@ -28,9 +28,7 @@ template <typename T> T min(T &a, T &b)
 	return (a < b ? a : b);
 }
 
-template <typename T>
-
-T max(T &a, T &b)
+template <typename T> T max(T &a, T &b)
 {
 	return (a > b ? a : b);
 }
